1def4a3b39afa858aa49b8dda77a3d8cd2181679
Arsh <arshsheth22@gmail.com>
2017-04-21 13:18:51 +0530

    Update table creation SCMS_Delivery_History_String.sql

--- SCMS/queries/table creation SCMS_Delivery_History_String.sql
+++ SCMS/queries/table creation SCMS_Delivery_History_String.sql
@@ -1,3 +1,22 @@
+--SCMS csv contains quoted "" data. We have to use SerDe to load quoted data.
+
+CREATE TABLE `scms_delivery.delivery_history` (
+  `ID` string ,`Project_Code` string ,`PQ` string ,`PO_or_SO` string ,
+  `ASNorDN` string ,`Country` string ,`Managed_By` string ,`Fulfill_Via` string ,
+  `Vendor_INCO_Term` string ,`Shipment_Mode` string ,`PQ_First_Sent_to_Client_Date` string ,
+  `PO_Sent_to_Vendor_Date` string ,`Scheduled_Delivery_Date` string ,
+  `Delivered_to_Client_Date` string ,`Delivery_Recorded_Date` string ,
+  `Product_Group` string ,`Sub_Classification` string ,`Vendor` string ,
+  `Item_Description` string ,`MoleculeTest_Type` string ,`Brand` string ,
+  `Dosage` string ,`Dosage_Form` string ,`Unit_of_Measure_Per_Pack` string ,
+  `Line_Item_Quantity` string ,`Line_Item_Value` string ,`Pack_Price` string ,
+  `Unit_Price` string ,`Manufacturing_Site` string ,`First_Line_Designation` string ,
+  `Weight_Kilograms` string ,`Freight_Cost_USD` string ,`Line_Item_Insurance_USD` string ) 
+  COMMENT "SCMS Delivery HISTORY" ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+  WITH SERDEPROPERTIES ("separatorChar"=",","quoteChar"="\"") STORED AS TextFile
+
+--The table below may not be loaded with consistent data
+
 CREATE EXTERNAL TABLE SCMS_Delivery_History_String(
 ID STRING,
 Project_Code STRING,
@@ -37,6 +56,8 @@ ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 STORED AS textfile
-LOCATION '/user/cloudera/hive/scms/'
+LOCATION '/user/cloudera/hive/scms/';
 
-load data local inpath '/home/cloudera/Desktop/ajinkya/datasets/supply-chain-dummy/SCMS_Delivery_History_Dataset_20150929.csv' into table scms_delivery_history_string;
+load data local inpath 
+'/home/cloudera/Desktop/ajinkya/datasets/supply-chain-dummy/SCMS_Delivery_History_Dataset_20150929.csv' 
+into table scms_delivery_history;
